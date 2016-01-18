e073df32b31560e9ae66aca20a7e336dc81189f7
niccokuc <nerminkuc74@gmail.com>
2016-01-18 21:56:29 +0400

    Change 3.0 : calc function

---
+++ NewPackage/app3.py
@@ -0,0 +1,7 @@
+def calc():
+    o =2
+    p = 2000
+    i=0
+    while i<2000:
+        i=i+10
+        print(i)
--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from NewPackage.app1 import funNewPackage
 from NewPackage.app2 import app2
+from NewPackage.app3 import calc
 
 print("app - master")
 print("app - master 1")
 funNewPackage()
 app2()
+calc()
 
 
